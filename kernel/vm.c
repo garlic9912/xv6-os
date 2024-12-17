ec4975ef3b71e373400f857df57c9b89255fc8bd
garlic9912 <2246257278@qq.com>
2024-12-17 16:44:16 +0800

    实现了vmprint函数,打印进程的页表信息

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void _vmprint(pagetable_t pagetable_t, int depth) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable_t[i];
+    if (pte & PTE_V) {
+      for (int i = 1; i <= depth; i++) {
+        printf("..");
+        if (i != depth) printf(" ");
+      }  
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        // this PTE points to a lower-level page table.
+        uint64 child = PTE2PA(pte);
+        _vmprint((uint64 *)child, depth+1);
+      }
+    }
+  }
+}
+
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 1);
+}
